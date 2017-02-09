bfbf035d492bcbb174edc188e020c8e0fca3cc42
Tanay <ariantanay2012@gmail.com>
2017-02-09 12:16:47 +0530

    Component folder renamed Client

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Bob from './components/Bob.jsx';
+import Bob from './client/Bob.jsx';
 
 
 ReactDOM.render(
